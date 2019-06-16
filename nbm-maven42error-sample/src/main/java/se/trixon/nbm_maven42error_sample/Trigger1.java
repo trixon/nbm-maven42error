539e2546f1520155692362ad3061cd282818a072
Patrik Karlström <patrik@trixon.se>
2019-06-16 09:47:17 +0200

    Provoke error

---
+++ nbm-maven42error-sample/src/main/java/se/trixon/nbm_maven42error_sample/Trigger1.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 Patrik Karlström.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.nbm_maven42error_sample;
+
+/**
+ *
+ * @author Patrik Karlström
+ */
+public class Trigger1 {
+
+    public Trigger1() {
+        org.openide.awt.Actions.forID("Window", "org.netbeans.modules.options.OptionsWindowAction");
+    }
+}
